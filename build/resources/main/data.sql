4b319948133c20849b6e4d7a17da91110f125a2c
neosome_ <daniildaresin@gmail.com>
2026-03-04 12:23:11 +0300

    uninstall

--- build/resources/main/data.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO faculty (name, color) VALUES ('Gryffindor', 'red');
-INSERT INTO faculty (name, color) VALUES ('Slytherin', 'green');
-
-INSERT INTO student (name, age) VALUES ('Harry Potter', 11);
-INSERT INTO student (name, age) VALUES ('Draco Malfoy', 11);
--- build/resources/main/scripts.sql
+++
@@ -1,9 +0,0 @@
-select * from student where age > 10 and age < 20;
-
-select student.name from student;
-
-select student.name like '%0%' from student;
-
-select * from student where age < id;
-
-select * from student order by age;
